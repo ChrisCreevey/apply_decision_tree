5904c4d3d555f25fc6a6540b8fca148cb0504670
Chris Creevey <chris.creevey@gmail.com>
2024-02-19 17:02:18 +0000

    Gene contribution to Phenotype calculation

--- apply_decision_tree.c
+++ apply_decision_tree.c
@@ -18,6 +18,7 @@
 /****** Define  ********/
 
 #define DESCRIPTION_LENGTH 100000
+#define MAXNUMPHENOTYPES 100
 
 #ifndef TRUE
 #define TRUE 1
@@ -44,7 +45,10 @@ struct node {
 	char left_condition[DESCRIPTION_LENGTH]; 		/* this holds the text version of the condition ">","<", ">=", etc... */
 	char right_condition[DESCRIPTION_LENGTH]; 		/* this holds the text version of the condition ">","<", ">=", etc... */
 
+	char phenotype[100];
+
 	int traversal_count; /* counter to record how many times we visited each noe fo the tree during assessment of genomes */
+	float phenotype_count; /* Records the model infomration for the leaves of the tree on how many genomes ended up at this point of the tree */
 
 	int arff_line_num;	/* holds the line number that the information */
 	float *freqs;		/* hold the frequencies of this gene familily across all the genomes in the arff file */
@@ -56,9 +60,10 @@ struct node {
 	} node_type;
 
 struct node **node_array = NULL;
-int numfields=0, nodecount=0, edgecount=0, num_genomes=0; 
-char **genome_names=NULL, **genomes=NULL;
-FILE *outfile = NULL, *path_outfile = NULL;
+int numfields=0, nodecount=0, edgecount=0, num_genomes=0, useGenomeNames = TRUE, numphenotypes=0, numgenes=0; 
+float **gene_pos_contrib_to_pheno = NULL, **gene_neg_contrib_to_pheno = NULL;
+char **genome_names=NULL, **genomes=NULL, **phenotypes, **genenames = NULL, file_stem[1000], outfile_name[10000];
+FILE *outfile = NULL, *path_outfile = NULL, *pos_contrib_file = NULL, *neg_contrib_file = NULL;
 
 
 
@@ -73,7 +78,11 @@ int is_genefam_in_tree(struct node *position, char *famname);
 void asses_genomes(struct node *position, int genome);
 void print_tree_traversal_counts(struct node *position);
 void print_newick_treefile(struct node *position, FILE *newickfile);
+void calculate_gene_pheno_contribtion(int gene_num);
+void count_all_phenos_pos(struct node *position, int gene_num);
+void count_all_phenos_neg(struct node *position, int gene_num);
 void print_tree_paths(struct node *position, char *path);
+void print_gene_contib_phenos(void);
 
 
 
@@ -82,18 +91,22 @@ void print_tree_paths(struct node *position, char *path);
  int main(int argc, char const *argv[]){
 
 	 FILE *treefile = NULL, *arff_file = NULL, *newickfile = NULL, *genomenamefile = NULL;
-	 int i;
-	 char command[10000] = "grep \"^N[0-9]\" ", path[1000000], outfile_name[10000], pathoutfile_name[10000];
+	 int i, j, found=FALSE;
+	 char command[10000] = "grep \"^N[0-9]\" ", path[1000000], pathoutfile_name[10000], node_phenotype[100];
 
-	 path[0] = '\0'; outfile_name[0] = '\0', pathoutfile_name[0] = '\0';
+	 path[0] = '\0'; outfile_name[0] = '\0'; pathoutfile_name[0] = '\0'; file_stem[0] ='\0';
 
-	 if(argc < 4)
+	 if(argc < 3)
 	 	{	
-	 	printf("apply_decision_tree\n\n\tUsage:\n\tapply_decision_tree <DOT-formmated tree file> <ARFF-formmated data file> <genome-names-file>\n\n");
+	 	printf("apply_decision_tree\n\n\tUsage:\n\tapply_decision_tree <DOT-formmated tree file> <ARFF-formmated data file> <OPTIONAL genome-names-file>\n\n");
 	 	printf("Output predictions for all genomes will be written to a file called <genome-names-file>.predictions.txt\n" );
 	 	printf("All paths though the decision tree will be written to a file called <genome-names-file>.paths.txt\n\n" );
 	 	exit(0);
 	 	}
+	 if(argc == 3)
+	 	useGenomeNames = FALSE;
+
+	 strcpy(file_stem, argv[1]);
 
 	 strcat(command, argv[1]);
 	 strcat(command, " | sed 's/\\[label=\"//g' | sed 's/\" *\\]//g'| sed 's/\\\".*\\]$//g' | sed \"s/\\(N[0-9]*\\) /\\1	/g\" | sed '/->/s/^/EDGE	/g' | sed '/^N[0-9]/s/^/NODE	/g' | awk '{print $1\"\t\"$2\"\t\"$3\"\t\"$4\"#\"}' > formatted_tree_file.txt");
@@ -114,14 +127,16 @@ void print_tree_paths(struct node *position, char *path);
  	fclose(arff_file);
 
     /* STEP 2.5 Read in the genome_names file */
- 	genomenamefile = fopen(argv[3], "r");
+ 	if(useGenomeNames){
+	 	genomenamefile = fopen(argv[3], "r");
 
- 	sprintf(outfile_name, "%s.predicitions.txt", argv[3]);
+	 	printf("Starting to read genome name file\n");
+	 	read_genome_names (genomenamefile);
+	 	fclose(genomenamefile);
+	 }
+
+ 	sprintf(outfile_name, "%s.predicitions.txt", file_stem);
  	outfile = fopen(outfile_name, "w");
-
- 	printf("Starting to read genome name file\n");
- 	read_genome_names (genomenamefile);
- 	fclose(genomenamefile);
 
  	/* STEP 3: assess all the genome information based on the decision tree */
  	printf("Printing Genome predicitons to %s\n", outfile_name);
@@ -137,17 +152,91 @@ void print_tree_paths(struct node *position, char *path);
 
  	/* STEP 5: print Newick formatted output of decision tree with traversal stats included */
  	printf("\nCreating Newick formatted version of decision tree\n");
- 	newickfile = fopen("newick_output.txt", "w");
+ 	sprintf(outfile_name, "%s.newick_output.txt", file_stem);
+ 	newickfile = fopen(outfile_name, "w");
  	fprintf(newickfile, "(");
  	print_newick_treefile(node_array[0], newickfile);
  	fprintf(newickfile, ");\n");
  	fclose(newickfile);
  	printf("\tNewick tree written to file newick_output.txt\n");
 
+ 	/* STEP 5.5 calculate contribution of each gene to each phenotype on the tree */
+
+ 	genenames=malloc(nodecount*sizeof(char *)); /* Allocate memory to hold the names of all the genes */
+ 	for(i=0; i<nodecount; i++){
+ 		genenames[i] = malloc(1000*sizeof(char));
+ 		genenames[i][0] = '\0';
+ 		}
+
+
+ 	phenotypes=malloc(MAXNUMPHENOTYPES*sizeof(char *));
+ 	for(i=0; i<=MAXNUMPHENOTYPES; i++)
+ 		{
+ 		phenotypes[i]=malloc(100*sizeof(char));
+ 		phenotypes[i][0] = '\0';
+ 		}
+ 		/* Count how many phenotypes are in the tree */
+ 	for(i=0; i<nodecount; i++){
+ 		if(node_array[i]->left == NULL && node_array[i]->right == NULL) /* this is a terminal node (leaf) */
+ 			{
+ 			strcpy(node_array[i]->phenotype, strtok(node_array[i]->label, " ")); /* Get the phenotype */
+ 			node_array[i]->phenotype_count = atof(strtok(NULL, "( /)")); /* capture the phenotype count on this leaf */
+ 			found=FALSE;
+ 			for(j=0; j<numphenotypes; j++) /* Look to see if this is the first time we've seen this phenotype */
+ 				{
+ 				if(strcmp(node_array[i]->phenotype, phenotypes[j])== 0)
+ 						found=TRUE;
+ 				}
+ 			if(!found){ /* If this is the first time we've seen this phenotype */
+ 				strcpy(phenotypes[numphenotypes], node_array[i]->phenotype);
+ 				numphenotypes++;
+ 				}
+			}
+		else  /* This is an internal node, so capture the gene name */
+			{
+			found = FALSE;
+			for(j=0; j<numgenes; j++) /* Look to see if this is the first time we've seen this gene */
+ 				{
+ 				if(strcmp(node_array[i]->label, genenames[j])== 0)
+ 						found=TRUE;
+ 				}
+ 			if(!found){ /* If this is the first time we've seen this gene */
+ 				strcpy(genenames[numgenes], node_array[i]->label);
+ 				numgenes++;
+ 				}
+			}
+ 		}
+
+ 	printf("Number of Phenotypes = %d\n", numphenotypes);
+ 	printf("phenos found:\t");
+ 	for(j=0; j<numphenotypes; j++) printf("%s\t", phenotypes[j]);
+ 	printf("\n");
+ 	printf("Genes found:\t");
+ 	for(j=0; j<numgenes; j++) printf("%s\t", genenames[j]);
+ 	printf("\n");
+ 
+ 	/* Assign the global arrays that record the values contribution of each gene to each phenotype */
+ 	gene_pos_contrib_to_pheno = malloc(numgenes*sizeof(float*));
+ 	gene_neg_contrib_to_pheno = malloc(numgenes*sizeof(float*));
+
+ 	for(i=0; i<numgenes; i++){
+ 		gene_pos_contrib_to_pheno[i] = malloc(numphenotypes*sizeof(float));
+ 		gene_neg_contrib_to_pheno[i] = malloc(numphenotypes*sizeof(float));
+ 		for(j=0; j<numphenotypes; j++){
+ 			gene_pos_contrib_to_pheno[i][j]=0;
+ 			gene_neg_contrib_to_pheno[i][j]=0; /* assign all values to zero to start */
+ 			}
+ 		}
+
+ 	for(j=0; j<numgenes; j++){  /* Now for each unique gene name, find all instnaces of it in the tree and calculate the toal contribution to each phenotype */
+	 	calculate_gene_pheno_contribtion(j);
+	 }
+
+	 print_gene_contib_phenos();
 
  	/* STEP 6 print all paths through trees (iE the paths to resistance) */
 
- 	sprintf(pathoutfile_name, "%s.paths.txt", argv[3]);
+ 	sprintf(pathoutfile_name, "%s.paths.txt", file_stem);
  	printf("Printing paths through Decision trees to %s\n", pathoutfile_name);
  	path_outfile = fopen(pathoutfile_name, "w");
 
@@ -172,6 +261,39 @@ void print_tree_paths(struct node *position, char *path);
 	}
 
 
+void print_gene_contib_phenos(void){
+
+	int i=0, j=0;
+	 /* Print out the resultsing counts for contribution of each gene to each phenotype */
+	 sprintf(outfile_name, "%s.Gene_presense_contrib_to_phenotype.txt", file_stem);
+	 pos_contrib_file = fopen(outfile_name, "w");
+	 sprintf(outfile_name, "%s.Gene_absense_contrib_to_phenotype.txt", file_stem);
+	 neg_contrib_file = fopen(outfile_name, "w");
+
+	 fprintf(pos_contrib_file, "Gene_name");  /* Print the header */
+	 fprintf(neg_contrib_file, "Gene_name");  /* Print the header */
+	 for(i=0; i<numphenotypes; i++) {
+	 	fprintf(pos_contrib_file, "\t%s", phenotypes[i]);
+	 	fprintf(neg_contrib_file, "\t%s", phenotypes[i]);
+	 	}
+	 fprintf(pos_contrib_file, "\n");
+	 fprintf(neg_contrib_file, "\n");
+
+	 /* For each gene print the total contributions to each phenotype */
+	 for(i=0; i<numgenes; i++){
+	 	fprintf(pos_contrib_file, "%s", genenames[i]);
+	 	fprintf(neg_contrib_file, "%s", genenames[i]);
+	 	for(j=0; j<numphenotypes; j++){
+	 		fprintf(pos_contrib_file, "\t%f", gene_pos_contrib_to_pheno[i][j]);
+	 		fprintf(neg_contrib_file, "\t%f", gene_neg_contrib_to_pheno[i][j]);
+	 		}
+		fprintf(pos_contrib_file, "\n");
+		fprintf(neg_contrib_file, "\n");
+	 	}
+	 fclose(pos_contrib_file);
+	 fclose(neg_contrib_file);
+
+	}
 
 void print_tree_details(struct node *position)
 	{
@@ -229,6 +351,7 @@ void read_tree (FILE * treefile)
  		node_array[i]->name=i;
  		node_array[i]->label=malloc(DESCRIPTION_LENGTH*sizeof(char));
  		node_array[i]->label[0]='\0';
+ 		node_array[i]->phenotype[0]='\0';
  		node_array[i]->left_value=0;
   		node_array[i]->right_value=0;
  		node_array[i]->left_condition[0]='\0';
@@ -503,8 +626,11 @@ void asses_genomes(struct node *position, int genome)
 		}
 
 	if(position->left == NULL && position->right == NULL)
-		{	
-		fprintf(outfile, "%s\t%d\t%d\t%s\n", genomes[genome], genome, position->name, position->label);
+		{
+		if(useGenomeNames)	
+			fprintf(outfile, "%s\t%d\t%d\t%s\n", genomes[genome], genome, position->name, position->label);
+		else
+			fprintf(outfile, "%d\t%d\t%s\n", genome, position->name, position->label);
 		}
 
 	}
@@ -579,12 +705,15 @@ void print_newick_treefile(struct node *position, FILE *newickfile)
 		}
 	if(position->parent != NULL)  /* Go up one level in the tree to get the condition which results in a genome coming to this node */
 		{	
-		if(position->parent->right == position)
-			sprintf(tmpstring, "%s%f", position->parent->right_condition, position->parent->right_value);
-		else
-			sprintf(tmpstring, "%s%f", position->parent->left_condition, position->parent->left_value);
+		if(position->parent->right == position){
+				sprintf(tmpstring, "%s%f", position->parent->right_condition, position->parent->right_value);
+				
+			}
+		else{
+				sprintf(tmpstring, "%s%f", position->parent->left_condition, position->parent->left_value);
+				
+			}
 		}
-
 	if(position->right != NULL) 
 		{
 		print_newick_treefile(position->right, newickfile);
@@ -598,6 +727,82 @@ void print_newick_treefile(struct node *position, FILE *newickfile)
 	free(newlabel);
 
 	}
+
+
+/* This fuinction is provided a gene name, then it does two steps. */
+/* 1) find all nodes (one by one) that have this gene name in them */
+/* 2) for each of the nodes calculates the Contributions to each phenotype and adde to the global arrays */
+
+
+void calculate_gene_pheno_contribtion(int gene_num)  
+	{
+	int i;
+
+	for(i=0; i<nodecount; i++){
+		if(strcmp(node_array[i]->label, genenames[gene_num]) == 0 ){
+			if(strchr(node_array[i]->left_condition, '<') != NULL){
+				count_all_phenos_neg(node_array[i]->left, gene_num);
+			}else{
+				if(strchr(node_array[i]->left_condition, '>') != NULL){
+					count_all_phenos_pos(node_array[i]->left, gene_num);
+					}
+				else{
+					printf("warning: INTERNAL NODE %s DOESN'T HAVE AN OBVIOUS '>' OR '<' LEFT CONDITION, this will not be counted\n", genenames[gene_num]);
+					}
+				}
+			if(strchr(node_array[i]->right_condition, '<') != NULL){
+				count_all_phenos_neg(node_array[i]->right, gene_num);
+			}else{
+				if(strchr(node_array[i]->right_condition, '>') != NULL){
+					count_all_phenos_pos(node_array[i]->right, gene_num);
+					}
+				else{
+					printf("warning: INTERNAL NODE %s DOESN'T HAVE AN OBVIOUS '>' OR '<' RIGHT CONDITION, this will not be counted\n", genenames[gene_num]);
+					}
+				}
+			
+			}
+
+		}
+
+	
+	}
+
+/* This is called by calculate_gene_pheno_contribtion and sums all phenotype contributions */	
+void count_all_phenos_pos(struct node *position, int gene_num)
+	{
+	int i=0;
+	if(position->right != NULL) 
+			count_all_phenos_pos(position->right, gene_num);
+		
+	if(position->left != NULL)
+		count_all_phenos_pos(position->left, gene_num);
+
+	if(position->left == NULL && position->right == NULL){
+		i=0;
+		while(strcmp(position->phenotype, phenotypes[i]) != 0)i++;
+		gene_pos_contrib_to_pheno[gene_num][i]+=position->phenotype_count;
+		}
+	}
+
+
+/* This is called by calculate_gene_pheno_contribtion and sums all phenotype contributions */	
+void count_all_phenos_neg(struct node *position, int gene_num)
+	{
+	int i=0;
+	if(position->right != NULL) 
+		count_all_phenos_pos(position->right, gene_num);
+		
+	if(position->left != NULL)
+		count_all_phenos_pos(position->left, gene_num);
+
+	if(position->left == NULL && position->right == NULL){
+		i=0;
+		while(strcmp(position->phenotype, phenotypes[i]) != 0)i++;
+		gene_neg_contrib_to_pheno[gene_num][i]+=position->phenotype_count;
+		}
+	}
+
 
 void read_genome_names (FILE * genome_names_file)
 	{
@@ -618,6 +823,6 @@ void read_genome_names (FILE * genome_names_file)
 		else{
 			fscanf(genome_names_file, "%s\n", genomes[i]);
 			}
-		i++; 
+		i++;
 		}
 	}
